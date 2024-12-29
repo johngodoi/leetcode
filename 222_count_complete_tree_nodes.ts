a43e1e3b87db22b6d211f9df41112933941099d4
John Godoi <john.godoi@gmail.com>
2024-12-29 09:19:59 -0300

    feat: 222

---
+++ 222_count_complete_tree_nodes.ts
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countNodes(root: TreeNode | null): number {
+    if(root == null) return 0;
+    if(root.right == null && root.left == null) return 1;
+    return countNodes(root.right) + countNodes(root.left) + 1;
+};
